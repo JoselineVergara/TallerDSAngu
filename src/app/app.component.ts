39fbc937a6857d2448a6b1098385cdd5d4bde407
Josie <joselinevergara@uees.edu.ec>
2025-02-06 11:03:21 -0500

    Creacion de componentes
    Se creo el componente footer y se modifico el componente inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { ContactoComponent } from './shared/contacto/contacto.component';
 import { CartaComponent } from './shared/carta/carta.component';
 import { InicioComponent } from './shared/inicio/inicio.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactoComponent,CartaComponent,InicioComponent],
+  imports: [RouterOutlet, ContactoComponent,CartaComponent,InicioComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+  back:String="Back to top"
+  context:String="Album example is &copy; Bootstrap, but please download and customize it for yourself!"
+
+
+}
